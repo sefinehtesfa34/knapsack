0bb9fe597e26baebeff72e1aa99afc1d2a8bdf2e
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2022-06-17 00:14:41 +0300

    read text files and create a graph

--- hill_climbing_algorithm.py
+++ hill_climbing_algorithm.py
@@ -2,27 +2,31 @@ import random
 import os
 file_path = 'data.txt'
 max_weight = random.randint(30,100)
-if os.stat(file_path).st_size == 0:
+    #Random graph generator
+def generateProduct(size):
     with open('data.txt', 'a') as f:
         f.write(str(max_weight) + "\n")
         f.write("name, " + "weight, " + "value" + "\n")
 
-    #Random graph generator
-    def generateProduct(size):
-        for i in range(size):
-            weight = random.randint(1,25)
-            value = random.randint(5,30)
-            element1 = random.randint(65,90)
-            element2 = random.randint(97,122)
-            element3 = random.randint(97,122)
-            element4 = random.randint(97,122)
-            element5 = random.randint(97,122)
-            element6 = random.randint(97,122)
-            arr = [chr(element1), chr(element2), chr(element3), chr(element4), chr(element5), chr(element6)]
-            name = "".join(arr)
-            with open('data.txt', 'a') as f:
-                f.write(name + "," + str(weight) + "," + str(value) + "\n" )
-    generateProduct(20)
+    for i in range(size):
+        weight = random.randint(1,25)
+        value = random.randint(5,30)
+        element1 = random.randint(65,90)
+        element2 = random.randint(97,122)
+        element3 = random.randint(97,122)
+        element4 = random.randint(97,122)
+        element5 = random.randint(97,122)
+        element6 = random.randint(97,122)
+        arr = [chr(element1), chr(element2), chr(element3), chr(element4), chr(element5), chr(element6)]
+        name = "".join(arr)
+        with open('data.txt', 'a') as f:
+            f.write(name + "," + str(weight) + "," + str(value) + "\n" )
+
+if os.path.exists(file_path):
+    if os.stat(file_path).st_size == 0:
+        generateProduct(20)
+else:
+    generateProduct(20)    
 
 class HillClimbingAlgorith:
     def __init__(self,max_weight,items):
@@ -62,10 +66,22 @@ class HillClimbingAlgorith:
         print("\nThe Optimal solution for knapsack problems according to Hill Climbing algorithm is as follows: \n")
         for item,weight,value in best_fit:
             print(item)
-items=[("A",10,30),("B",40,30),("C",40,10),("D",10,25),("E",5,18)]
+items=[]
+with open("data.txt","r") as text_file:
+    line_reader=text_file.readlines()
+    for line in line_reader:
+        line_list=line.strip().split(",")
+        try:
+            item=line_list[0]
+            weight=int(line_list[1])
+            value=int(line_list[2])
+            items.append((line_list[0],weight,value))
+        except:
+            continue
 instance=HillClimbingAlgorith(50,items)
 instance.candidate_evaluator()
-
+        
+    
 
 
 
