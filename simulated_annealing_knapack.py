948f13850ea8936d3ce9da9b23676843b64f0a0a
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2022-06-17 22:47:43 +0300

    rename the function names to make it looks nice

--- simulated_annealing_knapack.py
+++ simulated_annealing_knapack.py
@@ -24,7 +24,7 @@ def calculate_the_value(x): #calculate the value of the backpack
         sum_of_values += x[index]*value[index] 
         sum_weights += x[index]*weight[index]    
     return sum_of_values
-def random_solution_geberator(): #Initially generate random solutions
+def random_solution_geberator(): #mainially generate random solutions
     while True:
         for index in range(number_of_items):
             if(random.random() <0.5): current_solution[index]=1
@@ -36,66 +36,65 @@ def random_solution_geberator(): #Initially generate random solutions
     best=calculate_the_value(current_solution)
     copy_the_current_arry_to_another_array(best_solution,current_solution,number_of_items)
 
-def init(): #initialization function
-    global max_capacity,best,temperature
-    max_capacity = 269
-    best=-1
-    random_solution_geberator() #random_solution_geberator initial solution
-def get(x): #Randomly take out the items that already exist in the backpack
+def get(best_solution): #Randomly take out the items that already exist in the backpack
     while(1>0):
-        ob = random.randint(0,number_of_items-1)
-        if(x[ob]==1): 
-            x[ob]=0
+        random_neighbor= random.randint(0,number_of_items-1)
+        if(best_solution[random_neighbor]==1): 
+            best_solution[random_neighbor]=0
             break
-def put(x): #Randomly put items that do not exist in the backpack
+def put(best_solution): #Randomly put items that do not exist in the backpack
     while True:
-        ob = random.randint(0,number_of_items-1)
-        if(x[ob]==0): 
-            x[ob]=1
+        random_neighbor = random.randint(0,number_of_items-1)
+        if(best_solution[random_neighbor]==0): 
+            best_solution[random_neighbor]=1
             break       
-def slove(): #iterative function
+def compute_the_best_solut(): 
     global best,temperature,balance
     test=[0]*number_of_items
     current = 0 #Current backpack value
     for _ in range(balance):
         current = calculate_the_value(current_solution)
         copy_the_current_arry_to_another_array(test,current_solution,number_of_items)
-        ob = random.randint(0,number_of_items-1) #Randomly select an item
-        if(test[ob]==1): 
+        random_neighbor = random.randint(0,number_of_items-1) #Randomly select an item
+        if(test[random_neighbor]==1): 
             put(test)
-            test[ob]=0 #Take it out in the backpack and add other items
+            test[random_neighbor]=0 #Take it out in the backpack and add other items
         else: #If not in the backpack, directly add or replace the items in the backpack
             if(random.random()<0.5):
-                test[ob]=1 
+                test[random_neighbor]=1 
             else: 
                 get(test)
-                test[ob]=1
+                test[random_neighbor]=1
         temp = calculate_the_value(test)
         if(sum_weights>max_capacity):
             continue # skip if illegal solution
         if(temp> best): 
             best=temp 
-            copy_the_current_arry_to_another_array(best_solution,test,number_of_items) #Update the global best
-        
+            copy_the_current_arry_to_another_array(best_solution,test,number_of_items) 
         if(temp> current): 
             copy_the_current_arry_to_another_array(current_solution,test,number_of_items) #accept the new solution directly 
         else:
             g = 1.0*(temp-current)/temperature
             if(random.random() <math.exp(g)): #Probability to accept inferior solutions
                 copy_the_current_arry_to_another_array(current_solution,test,number_of_items)    
-init()
-isGood = False 
+def main(): #mainialization function
+    global max_capacity,best,temperature
+    max_capacity = 269
+    best=-1
+    random_solution_geberator() #random_solution_geberator initial solution
+
+main()
+best_selected = False 
 for i in range(time):      
-    slove()
-    temperature = temperature*annealing_rate #temperature drop
+    compute_the_best_solut()
+    temperature = temperature*annealing_rate #decrease the temprature
     if(best==295):  
-        print('Find the optimal solution: 295, the number of iterations', i+1) 
-        isGood = True 
-        break #reach the optimal solution and exit early
+        best_selected = True 
+        break #reach the optimal solution
         
-if(isGood == False): 
+if(best_selected == False): 
     print(best)
-print('The selected items are :',list(map(bool,best_solution))) #Print scheme
+print('The selected items are :',list(map(bool,best_solution)))
 val=0
 weigh=0
 for index,is_selected in enumerate(best_solution):
